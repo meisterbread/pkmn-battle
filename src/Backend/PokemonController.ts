3f648bea2144eb5b1b8ab5c5023e32d33886b749
meisterbread <justinebenedictuamli@gmail.com>
2024-09-25 22:22:01 +0800

    Add random opponent

--- src/Backend/PokemonController.ts
+++ src/Backend/PokemonController.ts
@@ -1,14 +1,14 @@
 import { Request,Response } from "express"
 import { PokemonModel } from "./PokemonModel"
 
-// controller = functions
+const url = `https://pokeapi.co/api/v2/pokemon/`
 
 const fetchPokemon = async(req : Request, res : Response) => {
-    const pokemonName = req.params.pokemonName
+    const pokemonName : string = req.params.pokemonName
 
     try{
-        const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-        const response = await fetch(`${url}`)
+
+        const response = await fetch(`${url}${pokemonName}`)
     
         const data = (await response.json()) as PokemonModel
     
@@ -19,4 +19,36 @@ const fetchPokemon = async(req : Request, res : Response) => {
 
 }
 
-export {fetchPokemon}
+const randomPokemon = async(req: Request, res: Response) => {
+    try{
+        const id : Number = Math.floor(Math.random() * 1025) + 1
+        console.log(id)
+
+        const response = await fetch(`${url}${id}`)
+    
+        const data = (await response.json()) as PokemonModel
+    
+        res.json(data)
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+// const randomPokemon = async(req : Request, res : Response) => {
+//     try{
+//         const id : number = Math.floor(Math.random() * 1025) + 1
+//         const idString = id.toString()
+//         console.log(id)
+
+//         const test: number = 1
+
+//         const response = await fetch(`${url}${test}`)
+//         const data = (await response.json()) as PokemonModel
+
+//         res.json(data)
+//     }catch(e){
+//         console.log(e)
+//     }
+// }
+
+export {fetchPokemon, randomPokemon}
